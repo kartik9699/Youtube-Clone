e1665fff847b6fcae548fd9605ee80b062a1804f
kartik9699 <kartikparab300@gmail.com>
2026-08-02 23:29:04 +0530

    create the get all videos route

---
+++ Backend/Controller/comment.controller.js
@@ -0,0 +1,4 @@
+import Video from "../Model/videos.model.js";
+import User from "../Model/user.model.js";
+import Comment from "../Model/comment.model.js";
+import Channel from "../Model/channel.model.js";
