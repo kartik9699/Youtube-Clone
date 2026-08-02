f8e099a1832d7b4683324e190dae5a130f75512b
kartik9699 <kartikparab300@gmail.com>
2026-08-02 19:38:48 +0530

    conect database using mongoose added the cloud database

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
 const app=express();
 //server created using 3000 port
+mongoose.connect('mongodb+srv://kartikparab300_db_user:pzW8cHg7YF07jqKy@cluster0.vzsrw0z.mongodb.net/?appName=Cluster0')
+.then(()=>{
+    console.log("Database is connected");
+})
+.catch((err)=>{
+    console.log("Database is not connected");
+})
 app.listen(3000,()=>{
     console.log("server is started")
 })
