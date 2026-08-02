1291d8221bda4b5e1c88e0f1a81f42812f4bd174
kartik9699 <kartikparab300@gmail.com>
2026-08-02 23:35:48 +0530

    create route for single user

--- Backend/Controller/video.controller.js
+++ Backend/Controller/video.controller.js
@@ -16,3 +16,25 @@ export async function getAllVideos(req, res) {
     res.status(500).json({ message: "Error fetching videos" });
   }
 }
+//single video using id
+export async function getVideoById(req, res) {
+  try {
+    const { videoId } = req.params;
+
+    // 1. Find the video and increment the 'views' count by 1 atomically
+    const video = await Video.findByIdAndUpdate(
+      videoId,
+      { $inc: { views: 1 } },
+      { new: true } // Returns the updated document with the new view count
+    ).populate("uploader", "username profilePic");
+
+    if (!video) {
+      return res.status(404).json({ message: "Video not found" });
+    }
+
+    res.status(200).json(video);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error fetching video" });
+  }
+}
