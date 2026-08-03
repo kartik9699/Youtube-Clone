74c606ba55c1d5d317bd10279099ed3d85329cd0
kartik9699 <kartikparab300@gmail.com>
2026-08-03 20:10:18 +0530

    create routes for the comments

---
+++ Backend/Routes/comment.routes.js
@@ -0,0 +1,11 @@
+import { addComment,getVideoComments,updateComment,deleteComment } from "../Controller/comment.controller";
+import { verifyToken } from "../middleware/Auth.middleware";
+export function commentRoutes(app) {
+  app.post("/videos/:videoId/comments", verifyToken, addComment);
+
+  app.get("/videos/:videoId/comments", getVideoComments);
+
+  app.put("/comments/:commentId", verifyToken, updateComment);
+
+  app.delete("/comments/:commentId", verifyToken, deleteComment);
+}
